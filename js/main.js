965eb8b8cc79ac878df3befb177ab577ab5ce190
lucapolzone <lucapolzone@gmail.com>
2024-02-01 13:02:23 +0100

    feat: add bonus 1 & error handling on empty input message

--- js/main.js
+++ js/main.js
@@ -29,6 +29,9 @@ const app = createApp({
         done: false
       },
 
+      //variabile switch se il campo input è vuoto
+      emptyInput: false,
+      emptyInputMessage: ""
     }
   },
 
@@ -40,11 +43,30 @@ const app = createApp({
     
     // funzione per salvare il valore di input e aggiungerlo alle task
     addTask() {
-      //core della funzione
-      this.arrayTasks.push({ ...this.newTask });
-      this.newTask.text = '';
+      //se l'input è vuoto
+      if (!this.newTask.text) {
+        this.emptyInputMessage = true;
+
+        //error anche su index.html
+        this.emptyInputMessage = "error"
+        
+      } else {
+        if (this.newTask.text.trim()) {
+          //core della funzione
+          this.arrayTasks.push({ ...this.newTask });
+          this.newTask.text = '';
+        }
+      }
+        
+    
     }
-  
+    
+
+
+
+
+
+
   }
 });
 
